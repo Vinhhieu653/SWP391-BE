f47ae03b1aef52a4199c331e8bc3547b79bf020a
Vinhhieu653 <hieudvse172738@fpt.edu.vn>
2025-05-30 13:32:25 +0700

    feat: upload image with cloudinary

--- src/index.js
+++ src/index.js
@@ -11,11 +11,13 @@ import logoutRouter from './routes/auth/logout.route.js'
 import blogRoutes from './routes/blog/blog.route.js'
 import refreshTokenRouter from './routes/auth/refresh-token.route.js'
 import registerRouter from './routes/auth/register.route.js'
+import uploadRouter from './routes/upload-img/upload-img.route.js'
 import { basicAuth } from './middlewares/authSwagger.js'
 import { notFoundHandler, errorHandler } from './middlewares/api-response/responseUtils.js'
 import User from './models/data/user.model.js'
 import Role from './models/data/role.model.js'
 import Blog from './models/data/blog.model.js'
+import Image from './models/data/image.model.js'
 import { seedRoles } from './database/seeds/role.seed.js'
 import { seedUsers } from './database/seeds/users.seed.js'
 
@@ -51,6 +53,7 @@ app.use('/api/v1/auth', logoutRouter)
 app.use('/api/v1/auth', refreshTokenRouter)
 app.use('/api/v1/users', registerRouter)
 app.use('/api/v1/blogs', blogRoutes)
+app.use('/api/v1/upload', uploadRouter)
 
 // Xử lý lỗi
 app.use(notFoundHandler)
